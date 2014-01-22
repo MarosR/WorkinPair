08c48f90fe94247a4d0be830b4857517e2a85185
Marek Rusnak <maroos.toja@gmail.com>
2014-01-22 12:02:26 +0100

    End of project (add main)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include "utils.h"
+#include "generator.h"
+#include "min.h"
+#include "max.h"
+
+#include <iostream>
 int main()
 {
-   
+   int tab[10];
+   generuj(tab, 10);
+   showArrayValues(tab, 10);
+   std::cout<<"Minimum in tab is: "<<min(tab, 10)<<std::endl;
+   std::cout<<"Maximum in tab is: "<<max(tab, 10)<<std::endl;
    return 0;
 }
